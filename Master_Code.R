92dd46d331567a7e38dbf295de4bf5883ab96a10
Mikhail Mikhaylov <65683708+Mikhail773@users.noreply.github.com>
2021-03-18 21:48:51 +0000

    Update Master_Code.R

--- Master_Code.R
+++ Master_Code.R
@@ -1,10 +1,15 @@
 library(tidyverse)
-vehicles_original <- read_csv("/home/mikhail/Documents/Database/Project/vehicles.csv")
-View(vehicles_original)
-vehicles_edited <- select(vehicles_original, -2 & -5 & -16 & -18 & -21 & -24:-25) #-3
+vehicles <- read_csv("/home/mikhail/Documents/Database/Project/vehicles.csv")
+View(vehicles)
+vehicles$Date <- as.Date(vehicles$posting_date)
+vehicles$Time <- format(vehicles$posting_date,"%H:%M:%S")
+vehicles_edited <- select(vehicles, -2 & -5 & -16 & -18 & -21 & -24:-25 & -26) #-3
 View(vehicles_edited)
 View(vehicles_edited %>% count(posting_date))
 View(vehicles_edited %>% count(size))
 colSums(is.na(vehicles_edited))
-colnames(vehicles_original)
+colnames(vehicles)
 colnames(vehicles_edited)
+View(vehicles_edited %>% count(manufacturer))
+vehicles_edited
+
