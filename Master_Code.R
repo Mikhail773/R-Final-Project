98c9c77153e9185620b42b9688a39d0f42b636cc
Mikhail Mikhaylov <65683708+Mikhail773@users.noreply.github.com>
2021-03-19 03:09:00 +0000

    Update Master_Code.R

--- Master_Code.R
+++ Master_Code.R
@@ -1,10 +1,13 @@
 library(tidyverse)
+# Read vehicles.csv
 vehicles <- read_csv("/home/mikhail/Documents/Database/Project/vehicles.csv")
 View(vehicles)
+
+# Create new columns for date and time
 vehicles$Date <- as.Date(vehicles$posting_date)
 vehicles$Time <- format(vehicles$posting_date,"%H:%M:%S")
 # Remove "X1","id","url", "region"_,"url", "VIN", size", "Lat" , "Lon", "posting_date"
-vehicles_edited <- select(vehicles, -1:-3 & -5 & -16 & -18 & -21 & -24:-25 & -26) #-3
+vehicles_edited <- select(vehicles, -1:-3 & -5 & -16 & -18 & -21 & -24:-25 & -26) 
 View(vehicles_edited)
 View(vehicles_edited %>% count(posting_date))
 View(vehicles_edited %>% count(size))
@@ -13,6 +16,5 @@ colnames(vehicles)
 colnames(vehicles_edited)
 View(vehicles_edited %>% count(manufacturer))
 vehicles_edited
-row_number(vehicles_edited$Date)
+# Update Graph
 ggplot(vehicles_edited) + geom_bar(aes(manufacturer, fill = state))
-
