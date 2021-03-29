5034ad32f437b7e83dcdbc8ddad9965537182c4d
Mikhail Mikhaylov <65683708+Mikhail773@users.noreply.github.com>
2021-03-29 09:28:39 -0400

    Update Master_Code.R

--- Master_Code.R
+++ Master_Code.R
@@ -7,7 +7,7 @@ View(vehicles)
 vehicles$Date <- as.Date(vehicles$posting_date)
 vehicles$Time <- format(vehicles$posting_date,"%H:%M:%S")
 # Remove "X1","id","url", "region"_,"url", "VIN", size", "Lat" , "Lon", "posting_date"
-vehicles_edited <- select(vehicles, -1:-3 & -5 & -16 & -18 & -21 & -24:-25 & -26) 
+vehicles_edited <- select(vehicles, -(1:3) & -5 & -16 & -18 & -21 & -(24:26)) 
 View(vehicles_edited)
 View(vehicles_edited %>% count(posting_date))
 View(vehicles_edited %>% count(size))
